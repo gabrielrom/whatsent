086f275587117b00c99dd2d267da61fc05e42ece
Gabriel Matheus <gabrielpessoal626@gmail.com>
2023-04-11 19:56:20 -0300

    create header of signin screen

--- whatsent/Sources/App Sections/Login/SignIn/Views/SignInViewController.swift
+++ whatsent/Sources/App Sections/Login/SignIn/Views/SignInViewController.swift
@@ -11,23 +11,36 @@ import Foundation
 class SignInViewController: UIViewController {
     lazy var greeting: UILabel = {
         let label = UILabel()
+        let attributtedString = NSMutableAttributedString(string: "Faça seu login para começar uma experiência incrível.")
+        let paragraphStyle = NSMutableParagraphStyle()
         
-        label.text = "Hello, Gabriel!"
-        label.font = UIFont.systemFont(ofSize: 16)
-        label.textColor = .black
+        
+        label.numberOfLines = 2
+        label.font = UIFont(name: "Inter-Regular", size: 15)
+        label.textColor = UIColor(named: "Secondary-Text-Color")
         label.translatesAutoresizingMaskIntoConstraints = false
+        
+        paragraphStyle.lineSpacing = 5
+        attributtedString.addAttribute(.paragraphStyle,
+                                       value: paragraphStyle,
+                                       range: NSMakeRange(0, attributtedString.length))
+        
+        label.attributedText = attributtedString
         
         return label
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        
+        title = "Login"
+        view.backgroundColor = UIColor(named: "Screen-Background")
         view.addSubview(greeting)
         
         NSLayoutConstraint.activate([
-            greeting.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            greeting.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            greeting.widthAnchor.constraint(equalToConstant: 220),
+            greeting.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            greeting.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 15)
         ])
     }
 }
